8882be51744c36cb4fc9b4395ba4c75b4e743f51
Inés García <inesgarciadiezgd@gmail.com>
2023-05-22 17:25:26 +0200

    Solved someone errors

--- models/post.model.js
+++ models/post.model.js
@@ -1,5 +1,6 @@
 const getAll = () => {
-    return db.query('SELECT * FROM posts JOIN autores ON posts.AUTOR_id=autores.id');
+    return db.query(`SELECT post.id, titulo, descripcion, fecha_creacion, categoria, nombre, email, imagen FROM posts as post
+    INNER JOIN autores as autor ON post.AUTOR_id = autor.id`);
 }
 
 const getById = (AUTOR_id) => {
@@ -7,7 +8,12 @@ const getById = (AUTOR_id) => {
 }
 
 const getByAutorId = (AUTOR_id) => {
-    return db.query('SELECT * FROM posts JOIN autores ON posts.AUTOR_id=autores.id WHERE AUTOR_id = ?', [AUTOR_id]);
+    return db.query(
+        `SELECT post.id, titulo, descripcion, fecha_creacion, categoria, nombre, email, imagen FROM posts as post
+        INNER JOIN autores as autor ON post.AUTOR_id = autor.id
+        WHERE post.AUTOR_id = ?`,
+          [AUTOR_id]
+    );
 }
 
 const create = ({ titulo, descripcion, fecha_creacion, categoria, AUTOR_id}) => {
